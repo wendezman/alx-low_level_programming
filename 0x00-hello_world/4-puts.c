7afb968c0468b43ef5ded075b7152d9ca456ebb6
wendezman <wendezman@gmail.com>
2022-06-02 11:40:34 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ *main - entry point
+ *
+ *Return: 0 alwayes zero (succcess)
+ */
+int main(void) 
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
