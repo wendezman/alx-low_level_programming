a3a49e26f0a9f1b15915dfe69e1ae8dd3e4be4c2
wendezman <wendezman@gmail.com>
2022-06-07 17:32:00 -0700

    Times table 100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -22,6 +22,8 @@ void print_times_table(int n)
 				c = a * b;
 				if (c <= 99)
 					_putchar(' ');
+				if (c <= 9)
+					_putchar(' ');
 				if (c >= 100)
 				{
 					_putchar((c / 100) + '0');
