a790309456b82a68b9425acc425f73095d72550c
wendezman <wendezman@gmail.com>
2022-06-09 05:21:45 -0700

    0 > 972

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -23,7 +23,7 @@ int largest_number(int a, int b, int c)
 			largest = c;
 		}
 	}
-	else if (b > a)
+	else
 	{
 		if (b > c)
 		{
@@ -33,10 +33,6 @@ int largest_number(int a, int b, int c)
 		{
 			largest = c;
 		}
-	}
-	else
-	{
-		largest = c;
 	}	
 
 	return (largest);
