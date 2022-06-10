f339dc8150aca5d33eec17cdb7373d264c5db070
wendezman <wendezman@gmail.com>
2022-06-09 18:55:08 -0700

    Fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -3,37 +3,37 @@
 
 /**
  * main - prints the numbers from 1 to 100
+ *
  * Return: Always 0
  */
-
 int main(void)
 {
 	int m;
-	
+
 	for (m = 1; m <= 100; m++)
 	{
-		if ((m % 3 == 0) && (m % 5 == 0))
-		{
-			printf(" FizzBuzz");
-		}
-		else if ((m % 3 == 0) && (m % 5 != 0))
+		if (m % 3 == 0 && m % 5 != 0)
 		{
 			printf(" Fizz");
 		}
-		else if ((m % 5 == 0) && (m % 3 != 0))
+		else if (m % 5 == 0 && m % 3 != 0)
 		{
 			printf(" Buzz");
 		}
+		else if (m % 3 == 0 && m % 5 == 0)
+		{
+			printf(" FizzBuzz");
+		}
 		else if (m == 1)
 		{
-			printf("%f", m);
+			printf("%d", m);
 		}
 		else
 		{
-			printf(" %f", m);
+			printf(" %d", m);
 		}
 	}
 	printf("\n");
-	
+
 	return (0);
 }
