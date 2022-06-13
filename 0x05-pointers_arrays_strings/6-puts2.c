9e828f3511299f24c5a0cbd37e7fd072da4ef236
wendezman <wendezman@gmail.com>
2022-06-13 11:52:19 -0700

    Lies about me are not all true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts2 - prints every other character of a string
+ * @str: the string of which every other character is to be printed
+ */
+
+void puts2(char *str)
+{
+	int n, length;
+
+	length = strlen(str);
+
+	for (n = 0; n <= (length / 2); n++)
+	{
+		_putchar(s[2 * n]);
+	}
+	_putchar('\n');
+}
