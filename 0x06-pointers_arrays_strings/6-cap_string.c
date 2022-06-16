d58d0e8467b99f58c57a9f97758320a1582766db
wendezman <wendezman@gmail.com>
2022-06-16 06:30:54 -0700

    Capitalize on what comes

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @str: the string to be capitalized
+ * Return: the capitalized string
+ */
+
+char *cap_string(char *str)
+{
+int i;
+int l;
+int m;
+
+char n[] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] < 123 && str[i] > 96)
+		{
+			m = 0;
+
+			for (l = 0; m == 0 && l <= 12; l++)
+			{
+				if (str[i] == a[l])
+				{
+					m = 10;
+				}
+			}
+			if (m == 10)
+			{
+				str[i] = str[i] - 32;
+			}
+		}
+	}
+
+	return (str);
+}
