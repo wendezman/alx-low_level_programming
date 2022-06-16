7d9767fbef1b9a899b36ad6ca0c9bf758f0ab82a
wendezman <wendezman@gmail.com>
2022-06-16 03:01:44 -0700

    String comparison 0

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,7 +13,7 @@ int _strcmp(char *s1, char *s2)
 	int m;
 	int comp = 0;
 
-	for (m = 0; s1[m] != '\0' && s2[m] != '\0'; m++)
+	for (m = 0; s1[m] != '\0' && s2[m] != '\0' && comp == 0; m++)
 	{
 		comp = s1[m] - s2[m];
 	}
