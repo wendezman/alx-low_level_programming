65cfbd27c1f3b5f5e550629f82e349e75d52add0
wendezman <wendezman@gmail.com>
2022-06-16 02:46:52 -0700

    String comparison 0

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,7 +13,7 @@ int _strcmp(char *s1, char *s2)
 	int m = 0;
 	int comp = 0;
 
-	if (s1[m] != '\0' && s2[m] != '\0')
+	if (s1[m] != '\0' && s2[m] != '\0' && comp == 0)
 	{
 		comp = s1[m] - s2[m];
 		m++;
