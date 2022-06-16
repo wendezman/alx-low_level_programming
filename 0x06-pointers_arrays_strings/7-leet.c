d87738c14147dc885134f2ed0164a9610d3d97b0
wendezman <wendezman@gmail.com>
2022-06-16 08:44:04 -0700

    Mozart composed for everybody

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -12,16 +12,17 @@ char *leet(char *str)
 	char Upper[] = {'A', 'E', 'O', 'T', 'L'};
 	char Lower[] = {'a', 'e', 'o', 't', 'l'};
 	int m;
-	int n;
+	int n = 0;
 
 	for (m = 0; str[m] != '\0'; m++)
 	{
-		for (n = 0; n < 5; n++)
+		while (n < 5)
 		{
 			if (str[m] == Upper[n] || str[m] == Lower[n])
 			{
 				str[m] = c1337[n]
 			}
+			n++;
 		}
 	}
 	return (str);
