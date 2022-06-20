c36e252d0e4fab318dea57e49e36588676586da3
wendezman <wendezman@gmail.com>
2022-06-20 14:21:46 -0700

    Strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strpbrk - searches a string for any of a set of bytes
+ * @s: the string
+ * @accept: string of characters to be matched
+ * Return: pointer to matching character
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int n = 0;
+	unsigned int m = 0;
+
+	while (s[n] != '\0')
+	{
+		while (accept[m] != '\0')
+		{
+			if (s[n] == accept[m])
+			{
+				goto last;
+			}
+			m++;
+		}
+		n++;
+	}
+last: return (s[n] == accept[m] ? (s + n) : '\0');
+}
