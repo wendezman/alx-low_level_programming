f67d69a55742b469d209fbc8721bb5e65b67605e
wendezman <wendezman@gmail.com>
2022-06-22 11:24:26 -0700

    Recursion 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,7 +3,7 @@
 /**
  * _puts_recursion - prints a string, followed by a new line
  * @s: the string to be printed
- * Return: nothing
+ * Return: 1 and -1
  */
 
 void _puts_recursion(char *s)
@@ -11,7 +11,7 @@ void _puts_recursion(char *s)
 	if (*s == 0)
 	{
 		_putchar('\n');
-		return (0);
+		return;
 	}
 	_putchar(*s);
 	_puts_recursion(s + 1);
