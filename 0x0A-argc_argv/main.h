5ba234c0aad9b443c2ddb118298fc0a4ae6b3947
wendezman <103420357+wendezman@users.noreply.github.com>
2022-06-24 19:38:42 +0300

    Delete main.h

--- 0x0A-argc_argv/main.h
+++
@@ -1,7 +0,0 @@
-int _putchar(char c);
-int _whats_myname(int argc, char *argv[])
-int _args1(int argc, char *argv[])
-int _args2(int argc, char *argv[])
-int _mul(int argc, char *argv[])
-int _add(int argc, char *argv[])
-int _change(int argc, char *argv[])
