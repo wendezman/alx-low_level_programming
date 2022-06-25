00f4467d2e5dea42310f665ee15c367641720003
wendezman <wendezman@gmail.com>
2022-06-24 19:25:01 -0700

    Static library

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,20 +1,24 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * _putchar - prints character
+ * @c: the character
  * Return: return zero
  */
 
-int main(void)
+int _putchar(char c)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	_putchar (c);
+	/**
+	*_putchar('_');
+	*_putchar('p');
+	*_putchar('u');
+	*_putchar('t');
+	*_putchar('c');
+	*_putchar('h');
+	*_putchar('a');
+	*_putchar('r');
+	*/
 	_putchar('\n');
 
 	return (0);
